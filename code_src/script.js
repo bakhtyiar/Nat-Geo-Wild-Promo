6a9ede2394c0658246bb212bec1e278c9daa4388
Bakhtyiar Klimov <baxtiar00@gmail.com>
2022-08-08 03:46:16 +0300

    added dynamic counter number of articles. fixed scroll bug

--- code_src/script.js
+++ code_src/script.js
@@ -1,23 +1,35 @@
 let sliderArticlesWrapper = document.getElementById("articles-slider__articles");
 let sliderArticles = document.getElementsByClassName("articles-slider__article");
+let sliderArticlesAmount = document.querySelectorAll(".articles-slider__article").length;
+let sliderArticlesNumbers = document.querySelectorAll(".numbers__number");
+
+sliderArticlesNumbers[1].innerHTML = `0${sliderArticlesAmount}`;
 
 let sliderArticlesIndex = 0;
-let sliderArticlesPrevScrollTop = sliderArticlesWrapper.scrollTop;
 
 function scrollHandler(event) {
-	if(sliderArticlesPrevScrollTop != sliderArticlesWrapper.scrollTop) {
-		if (event.deltaY < 0) { // if scroll to up
-			sliderArticlesIndex = sliderArticlesIndex === 0 ? (sliderArticles.length) : (sliderArticlesIndex - 1);
+
+	if (sliderArticles.length < 2)
+		return ; //stop function if not enough articles to scroll
+
+	if (event.deltaY < 0) { // if scroll to up
+		if (sliderArticlesIndex === 0) {
+			sliderArticlesIndex = (sliderArticlesAmount - 1);
 			sliderArticles[sliderArticlesIndex].scrollIntoView({block: "start"});
-		} else if (event.deltaY > 0) { // if scroll to down
-			sliderArticlesIndex = sliderArticlesIndex === (sliderArticles.length) ? 0 : (sliderArticlesIndex + 1);
+		} else {
+			sliderArticlesIndex = (sliderArticlesIndex - 1);
+			sliderArticles[sliderArticlesIndex].scrollIntoView({block: "nearest"});
+		}
+	} else { // if scroll to down
+		if (sliderArticlesIndex === (sliderArticlesAmount - 1)) {
+			sliderArticlesIndex = 0;
 			sliderArticles[sliderArticlesIndex].scrollIntoView({block: "end"});
-		} else { // other variants
-			sliderArticlesIndex = sliderArticlesIndex === 0 ? (sliderArticles.length) : (sliderArticlesIndex - 1);
-			sliderArticles[sliderArticlesIndex].scrollIntoView({block: "start"});
+		} else {
+			sliderArticlesIndex = (sliderArticlesIndex + 1);
+			sliderArticles[sliderArticlesIndex].scrollIntoView({block: "nearest"});
 		}
 	}
-	sliderArticlesPrevScrollTop = sliderArticlesWrapper.scrollTop;
+	sliderArticlesNumbers[0].innerHTML = `0${sliderArticlesIndex + 1}`;
 };
 
 sliderArticlesWrapper.addEventListener("wheel", scrollHandler);
